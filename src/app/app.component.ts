67b03beb2415c017cc621ddc0c15c166c4fddc8e
jlong <jintao.long@evotec.com>
2024-08-17 17:00:47 +0100

    feat: App structure ini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarModule } from 'primeng/sidebar';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api/menuitem';
+import { DataViewModule } from 'primeng/dataview';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
+import { CommonModule } from '@angular/common';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+
+type Signal = {
+  name: string;
+  description: string;
+  category: string;
+  config: ChangeSignalConfig;
+}
+
+type ChangeSignalConfig = {
+  percentage: number;
+  isUp: boolean;
+  duration: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Default</h1>`,
+  imports: [
+    FormsModule,
+    DataViewModule,
+    ButtonModule,
+    TagModule,
+    CommonModule,
+    CheckboxModule,
+    InputGroupModule,
+    InputGroupAddonModule
+  ],
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'default';
+  // sidebarVisible = false;
+  // items: MenuItem[] = [
+  //   {title: "File", icon: "pi pi-fw pi-file"},
+  // ]
+  signals : Signal[] = [
+    {
+      name: "GBP/USD", 
+      description: "description1", 
+      category: "change", 
+      config: {
+        percentage: 0.01, isUp: true, duration: 3600
+      } as ChangeSignalConfig
+    } as Signal,
+    {
+      name: "USD/JPY", 
+      description: "description1", 
+      category: "change", 
+      config: {
+        percentage: 0.01, isUp: false, duration: 3600
+      } as ChangeSignalConfig
+    } as Signal,
+  ];
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@
 *  Protractor is used in this example for compatibility with Angular documentation tools.
 */
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
+import { NgModule } from '@angular/core';
 
 bootstrapApplication(AppComponent,
     {providers: [provideProtractorTestingSupport()]})
